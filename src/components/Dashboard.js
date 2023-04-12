b1a8a754b452b574612d510a6125f58c7be5cfa4
Stepan Klos <stepanklos@gmail.com>
2023-04-12 12:45:32 +0200

    Login user info - user further inspections

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -24,6 +24,7 @@ function Dashboard() {
     useEffect(() => {
         if (loading) return;
         if (!user) return navigate("/");
+        console.log(user)
         //fetchUserName();
     }, [user, loading]);
     return (
@@ -38,101 +39,98 @@ function Dashboard() {
             </style>
             <Container>
                 <Row>
-                    <Row>
-                        <Col>
-                            <h2>Places</h2>
-                            <span className="boxed">+</span>
-                            <Table className="boxed">
-                                <thead>
-                                    <tr>
-                                        <th>name</th>
-                                        <th>description</th>
-                                        <th>place [x,y]</th>
-                                        <th>actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>moje provozovna 1</td>
-                                        <td>place description 1</td>
-                                        <td>80.1276, 67.1768</td>
-                                        <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>moje provozovna 2</td>
-                                        <td>place description 2</td>
-                                        <td>81.5276, 68.8562</td>
-                                        <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>moje provozovna 3</td>
-                                        <td>place description 3</td>
-                                        <td>82.3476, 69.5262</td>
-                                        <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>moje provozovna 4</td>
-                                        <td>place description 4</td>
-                                        <td>81.9476, 70.02628</td>
-                                        <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>moje provozovna 5</td>
-                                        <td>place description 5</td>
-                                        <td>80.8976, 69.6682</td>
-                                        <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Col>
-                        <Col>
-                            <h2>Eshops</h2>
-                            <span className="boxed">+</span>
-                            <Table className="boxed">
-                                <thead>
-                                    <tr>
-                                        <th>name</th>
-                                        <th>description</th>
-                                        <th>url</th>
-                                        <th>actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>muj eshop 1</td>
-                                        <td>eshop pridany 1</td>
-                                        <td>www.mujeshop1.cz</td>
-                                        <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>muj eshop 2</td>
-                                        <td>eshop pridany 2</td>
-                                        <td>www.mujeshop2.cz</td>
-                                        <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>muj eshop 3</td>
-                                        <td>eshop pridany 3</td>
-                                        <td>www.mujeshop3.cz</td>
-                                        <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Col>
-                    </Row>
                     <Col>
-                        <Row>
-                            <div className="dashboard boxed">
-                                <div className="dashboard__container">
-                                    Logged in as
-                                    <div>{name}</div>
-                                    <div>{user?.email}</div>
-                                    <button className="dashboard__btn" onClick={logout}>
-                                        Logout
-                                    </button>
-                                </div>
+                        <div className="dashboard boxed">
+                            <div className="dashboard__container">
+                                <div>Logged in as <b>{user?.displayName}</b></div>
+                                <div>{user?.email} / {user?.uid}</div>
+                                <button className="dashboard__btn" onClick={logout}>
+                                    Logout
+                                </button>
                             </div>
-                        </Row>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h2>Places</h2>
+                        <span className="boxed">+</span>
+                        <Table className="boxed">
+                            <thead>
+                                <tr>
+                                    <th>name</th>
+                                    <th>description</th>
+                                    <th>place [x,y]</th>
+                                    <th>actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>moje provozovna 1</td>
+                                    <td>place description 1</td>
+                                    <td>80.1276, 67.1768</td>
+                                    <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
+                                </tr>
+                                <tr>
+                                    <td>moje provozovna 2</td>
+                                    <td>place description 2</td>
+                                    <td>81.5276, 68.8562</td>
+                                    <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
+                                </tr>
+                                <tr>
+                                    <td>moje provozovna 3</td>
+                                    <td>place description 3</td>
+                                    <td>82.3476, 69.5262</td>
+                                    <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
+                                </tr>
+                                <tr>
+                                    <td>moje provozovna 4</td>
+                                    <td>place description 4</td>
+                                    <td>81.9476, 70.02628</td>
+                                    <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
+                                </tr>
+                                <tr>
+                                    <td>moje provozovna 5</td>
+                                    <td>place description 5</td>
+                                    <td>80.8976, 69.6682</td>
+                                    <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                    <Col>
+                        <h2>Eshops</h2>
+                        <span className="boxed">+</span>
+                        <Table className="boxed">
+                            <thead>
+                                <tr>
+                                    <th>name</th>
+                                    <th>description</th>
+                                    <th>url</th>
+                                    <th>actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>muj eshop 1</td>
+                                    <td>eshop pridany 1</td>
+                                    <td>www.mujeshop1.cz</td>
+                                    <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
+                                </tr>
+                                <tr>
+                                    <td>muj eshop 2</td>
+                                    <td>eshop pridany 2</td>
+                                    <td>www.mujeshop2.cz</td>
+                                    <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
+                                </tr>
+                                <tr>
+                                    <td>muj eshop 3</td>
+                                    <td>eshop pridany 3</td>
+                                    <td>www.mujeshop3.cz</td>
+                                    <td><span className="boxed">EDIT</span><span className="boxed">DEL</span></td>
+                                </tr>
+                            </tbody>
+                        </Table>
                     </Col>
                 </Row>
             </Container >
