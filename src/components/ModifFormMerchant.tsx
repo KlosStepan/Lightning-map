df8c161a8f4e83dffa83a131d5033f6e46a03701
Stepan Klos <stepanklos@gmail.com>
2023-04-22 21:49:09 +0200

    Modif form Merchant prepared and refactored #1

--- src/components/ModifFormMerchant.tsx
+++ src/components/ModifFormMerchant.tsx
@@ -4,8 +4,9 @@ import { auth, db, logout } from "../components/Firebase"
 import { doc, getDoc } from "firebase/firestore";
 import { Container, Input, Table } from 'reactstrap';
 import { useNavigate } from 'react-router-dom';
-import IMerchant from '../ts/IMerchant';
+
 //TS
+import IMerchant from '../ts/IMerchant';
 interface IModifFormMerchantProps {
     edit?: boolean
     id?: string
@@ -14,50 +15,62 @@ interface IModifFormMerchantProps {
 function ModifFormMerchant(props: IModifFormMerchantProps = {}) {
     const [user, loading, error] = useAuthState(auth);
     const navigate = useNavigate();
-    //Debug - DELETABLE
-    console.log("ModifFormEshop props")
-    console.log(props)
-    ////Uncontrolled Form Handling via. ref& s
+    //console.log("<ModifFormMerchant /> props")
+    //console.log(props)
     const inputTitle = useRef<HTMLInputElement>(null);
     const inputDescription = useRef<HTMLInputElement>(null);
-    ////
     const [coordX, setCoordX] = useState<number>(15.1111);
     const [coordY, setCoordY] = useState<number>(51.2222);
-    //Data Handling Functions w/ Firebase
+
     const UpdateMerchant = () => {
-        console.log("UpdateMerchant()")
-    }
-    const AddMerchant = () => {
-        console.log("AddMerchant()")
-        const _merchant: IMerchant = { geometry: { coordinates: [coordX, coordY], type: "Point" }, properties: { description: inputDescription?.current!.value, owner: user?.uid, title: inputTitle?.current!.value }, type: "Feature" };
+        console.log(`UpdateMerchant(), Firebase -> UPDATE/${props.id}`)
+        const _merchant: IMerchant = _BundleMerchant();
         console.log(_merchant)
     }
-    const getMerchant = async (db: any, id: any) => {
-        const merchantId = id; // replace with the ID of the merchant you want to retrieve
+    const AddMerchant = () => {
+        console.log("AddMerchant(), Firebase -> INSERT")
+        const _merchant: IMerchant = _BundleMerchant();
+        console.log(_merchant)
+    }
+    const FetchMerchant = async (db: any, id: any) => {
+        const merchantId = id;
         const merchantRef = doc(db, 'merchants', merchantId);
         const merchantSnapshot = await getDoc(merchantRef);
         if (merchantSnapshot.exists()) {
             const merchantData = merchantSnapshot.data();
             //console.log(merchantData)
             return merchantData
-        } else {
+        }
+        else {
             console.log(`No merchant found with ID ${merchantId}`);
             return null
         }
     }
-    //Upon loading do
+    const _BundleMerchant = () => {
+        const _merchant: IMerchant = {
+            geometry: {
+                coordinates: [coordX, coordY],
+                type: "Point"
+            },
+            properties: {
+                description: inputDescription?.current!.value,
+                owner: user?.uid,
+                title: inputTitle?.current!.value
+            },
+            type: "Feature"
+        };
+        return _merchant
+    }
     useEffect(() => {
-        console.log("useEffect()")
+        console.log("<ModifFormMerchant /> useEffect()")
         if (props.edit) {
-            //if props.edit{ FETCH& load 4 fields }
-            console.log("edit")
-            console.log(props.id)
-            const merchant_promise = getMerchant(db, props.id)
+            //console.log("edit")
+            //console.log(props.id)
+            const merchant_promise = FetchMerchant(db, props.id)
             Promise.resolve(merchant_promise)
                 .then((result) => {
-                    console.log("result")
+                    console.log("Fetched merchant")
                     console.log(result)
-                    //
                     inputTitle.current!.value = result!.properties.title
                     inputDescription.current!.value = result!.properties.description
                     setCoordX(result!.geometry.coordinates[0])
@@ -69,51 +82,50 @@ function ModifFormMerchant(props: IModifFormMerchantProps = {}) {
         }
     })
     return (
-        <>
-            <Container>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th className='justWrap'><span className="btnStyle ptHover" onClick={() => { navigate(-1) }} > &lt; BACK </span></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th className='justWrap'>TITLE</th>
-                            <td>
-                                <Input
-                                    type="textarea"
-                                    rows="1"
-                                    innerRef={inputTitle}
-                                //value={(formKod) ? formKod : ""}
-                                //onChange={(e: any) => { setFormKod(e.target.value) }}
-                                />
-                            </td>
-                        </tr>
-                        <tr>
-                            <th className='justWrap'>DESCRIPTION</th>
-                            <td>
-                                <Input
-                                    type="textarea"
-                                    rows="5"
-                                    innerRef={inputDescription}
-                                />
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>place</th>
-                            <td>map box to pick x <b>{coordX}</b> and y <b>{coordY}</b></td>
-                        </tr>
-                    </tbody>
-                </Table>
-                <div className="alignCenter">
-                    {(props.edit)
-                        ? <span className="btnStyle ptHover" onClick={() => { UpdateMerchant() }} > UPDATE </span>
-                        : <span className="btnStyle ptHover" onClick={() => { AddMerchant() }} > ADD </span>}
-                </div>
-            </Container>
-        </>
+        <Container>
+            <Table>
+                <thead>
+                    <tr>
+                        <th className='justWrap'><span className="btnStyle ptHover" onClick={() => { navigate(-1) }} > &lt; BACK </span></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th className='justWrap'>TITLE</th>
+                        <td>
+                            <Input
+                                type="textarea"
+                                rows="1"
+                                innerRef={inputTitle}
+                            //value={(formKod) ? formKod : ""}
+                            //onChange={(e: any) => { setFormKod(e.target.value) }}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th className='justWrap'>DESCRIPTION</th>
+                        <td>
+                            <Input
+                                type="textarea"
+                                rows="5"
+                                innerRef={inputDescription}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>place</th>
+                        <td>map box to pick x <b>{coordX}</b> and y <b>{coordY}</b></td>
+                    </tr>
+                </tbody>
+            </Table>
+            <div className="alignCenter">
+                {(props.edit)
+                    ? <span className="btnStyle ptHover" onClick={() => { UpdateMerchant() }} > UPDATE </span>
+                    : <span className="btnStyle ptHover" onClick={() => { AddMerchant() }} > ADD </span>}
+            </div>
+            <span>&nbsp;</span>
+        </Container>
     )
 }
 export default ModifFormMerchant
