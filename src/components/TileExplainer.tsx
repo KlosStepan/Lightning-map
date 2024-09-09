b70b0c16411e44b79b517d0cc4384fc644741388
Stepan Klos <stepanklos@gmail.com>
2024-09-09 14:19:44 +0200

    Redesign sprint 43 - WhyLightning + adjustments

--- src/components/TileExplainer.tsx
+++ src/components/TileExplainer.tsx
@@ -1,56 +1,77 @@
 import Box from '@mui/material/Box';
-import { Card, CardMedia, Grid } from '@mui/material';
-import { Container, CssBaseline, Paper } from "@mui/material";
-
+import { CardMedia } from '@mui/material';
+import { Container } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
-
-//Down 3 boxes for LN info
+// Styles for the outer and inner containers
 const containerOuterStyle = {
-    //width: '315px',
-    //height: '478px',
-    padding: '32px 0px 10px 0px',
-    gap: '10px',
-    borderRadius: '24px 24px 24px 24px',
-    opacity: '0px',
-    backgroundColor: 'white', // Adding background color
-    margin: '0px 0px 10px 0px',
+  padding: '32px 0px 10px 0px',
+  gap: '10px',
+  borderRadius: '24px',
+  backgroundColor: 'white',
+  margin: '0px 0px 10px 0px',
 };
 
 const containerInnerStyle = {
-    gap: '20px',
-    opacity: '0px',
+  gap: '20px',
+  opacity: '0px',
 };
 
 // Define the type for the props
 type TilePitchProps = {
-    image: string;
-    title: string;
-    paragraph: string;
+  image?: string;
+  title: string;
+  paragraph: string;
+  breakImageAndTitle?: boolean; // New prop to control the layout
 };
 
-
 // Define the component using the defined props type
-const TileExplainer: React.FC<TilePitchProps> = ({ image, title, paragraph }) => {
-    return (
-        <Container maxWidth="sm" sx={containerOuterStyle}>
-            <Box sx={{ bgcolor: '#ffffff', ...containerInnerStyle }}>
-                <div style={{ width: 40, height: 40 }}>
-                    <CardMedia
-                        component="img"
-                        image={image}
-                        alt="Transaction Speed"
-                        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                    />
-                </div>
-                <Typography variant="h2" component="h2">
-                    {title}
-                </Typography>
-                <p style={{ /*border: '1px solid black', padding: '10px' */ }}>
-                    {paragraph}
-                </p>
-            </Box>
-        </Container>
-    )
-}
+const TileExplainer: React.FC<TilePitchProps> = ({
+  image,
+  title,
+  paragraph,
+  breakImageAndTitle = true, // Default value set to true
+}) => {
+  return (
+    <Container maxWidth="sm" sx={containerOuterStyle}>
+      <Box sx={{ bgcolor: '#ffffff', ...containerInnerStyle }}>
+        {breakImageAndTitle ? (
+          <>
+            {image && (
+              <div style={{ width: 40, height: 40 }}>
+                <CardMedia
+                  component="img"
+                  image={image}
+                  alt="Transaction Speed"
+                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                />
+              </div>
+            )}
+            <Typography variant="h2" component="h2">
+              {title}
+            </Typography>
+          </>
+        ) : (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {image && (
+              <div style={{ width: 40, height: 40, marginRight: '8px' }}>
+                <CardMedia
+                  component="img"
+                  image={image}
+                  alt="Transaction Speed"
+                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                />
+              </div>
+            )}
+            <Typography variant="h2" component="h2">
+              {title}
+            </Typography>
+          </div>
+        )}
+        <p>{paragraph}</p>
+      </Box>
+    </Container>
+  );
+};
+
 export default TileExplainer;
--- src/pages/WhyLightning.tsx
+++ src/pages/WhyLightning.tsx
@@ -1,16 +1,214 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Grid, Typography, CardMedia, Box } from "@mui/material";
 import Footer from "../components/Footer";
-import MiddleOfHomepage from "../components/MiddleOfHomepage";
-
+//import MiddleOfHomepage from "../components/MiddleOfHomepage";
+import TileExplainer from "../components/TileExplainer";
 //
-function WhyLightning() {
+import transactionspeed from '../img/Interface-Essential-Flash--Streamline-Pixel.png';
+import lowfees from '../img/Business-Products-Cash-User-Man-Message--Streamline-Pixel.png';
+import scalability from '../img/Business-Product-Startup-1--Streamline-Pixel.png';
+import security from '../img/Interface-Essential-Lock--Streamline-Pixel-2.png';
+import privacy from '../img/Coding-Apps-Websites-Shield-Lock--Streamline-Pixel.png'
+
+// Importing images
+import img1_magnet from '../img/Interface-Essential-Magnet--Streamline-Pixel.png';
+import imgs2Overlap1 from '../img/rectangle_145.png'; // Image 145
+import imgs2Overlap2 from '../img/rectangle_147.png'; // Image 147
+import img3Comeback from '../img/Hand-Love--Streamline-Pixel.png';
+
+
+// Define the common height
+const COMMON_HEIGHT = '450px';
+
+type WhyLightningProps = {};
+
+const WhyLightning: React.FC<WhyLightningProps> = () =>{
+//function WhyLightning() {
     return (
         <React.Fragment>
-            <Typography variant="h1" >How Bitcoin Lightning Revolutionizes Transaction Times</Typography>
-            {/*<MiddleOfHomepage/>*/}
-            <div>TODO - different arrangment</div>
-            <Footer />
+        <Box sx={{ minHeight: COMMON_HEIGHT, display: 'flex', alignItems: 'center' }}>
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          {/* First 1 unit */}
+          <Grid item xs={1}>
+            <div
+              style={{
+                position: "relative",
+                height: COMMON_HEIGHT,
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={img1_magnet}
+                alt="Magnet Image"
+                style={{
+                  width: '90px', // 77% of 90px
+                  height: '90px', // 77% of 90px
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  objectFit: 'cover',
+                }}
+              />
+            </div>
+          </Grid>
+
+          {/* First x/2 units */}
+          <Grid item xs={5}>
+            <div style={{ /*backgroundColor: "#90caf9", height: "100px"*/ }}>
+            <Typography variant="h1" component="h1" sx={{ fontSize: '2.5rem' }}>
+            Bitcoin Lightning Network is a <br/>
+            second-layer technology <br/>
+            designed to facilitate faster and <br/>
+            cheaper transactions on the <br/>
+            Bitcoin blockchain. <br/>
+            </Typography>
+            </div>
+          </Grid>
+
+
+          {/* Second x/2 units */}
+          <Grid item xs={5}>
+            <div style={{ position: "relative", height: COMMON_HEIGHT }}>
+              {/* Image 145 (imgs2Overlap1) scaled to 77% of original size */}
+              <CardMedia
+                component="img"
+                image={imgs2Overlap1}
+                alt="Image 145"
+                style={{
+                  width: '242px', // 77% of 315px
+                  height: '385px', // 77% of 500px
+                  position: 'absolute',
+                  bottom: 0,
+                  left: 0,
+                  objectFit: 'cover',
+                  zIndex: 2, // Ensures this image is on top of the other where they overlap
+                }}
+              />
+              {/* Image 147 (imgs2Overlap2) scaled to 77% of original size */}
+              <CardMedia
+                component="img"
+                image={imgs2Overlap2}
+                alt="Image 147"
+                style={{
+                  width: '199px', // 77% of 259px
+                  height: '308px', // 77% of 400px
+                  position: 'absolute',
+                  top: 0,
+                  right: 0,
+                  objectFit: 'cover',
+                  zIndex: 1, // This will be under Image 145 in overlapping areas
+                }}
+              />
+            </div>
+          </Grid>
+
+          {/* Last 1 unit */}
+          <Grid item xs={1}>
+            <div
+              style={{
+                position: "relative",
+                height: COMMON_HEIGHT,
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={img3Comeback}
+                alt="Comeback Image"
+                style={{
+                  width: '90px', // 77% of 90px
+                  height: '90px', // 77% of 90px
+                  position: 'absolute',
+                  bottom: 0,
+                  right: 0,
+                  objectFit: 'cover',
+                }}
+              />
+            </div>
+          </Grid>
+        </Grid>
+        </Box>
+        <React.Fragment>
+            <div>&nbsp;</div>
+        </React.Fragment>
+        <React.Fragment>
+                <Grid container spacing={2}>
+                    <Grid item xs={3}>
+                        <Typography variant="h1" component="h2">
+                            How it works
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <TileExplainer
+                            //image={transactionspeed}
+                            title="Opening a Channel"
+                            paragraph="Two parties open a payment channel by creating a multi-signature address on the Bitcoin blockchain. This address holds a certain amount of Bitcoin deposited by both parties."
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <TileExplainer
+                            //image={lowfees}
+                            title="Conducting Transactions"
+                            paragraph="Once the channel is open, the parties can conduct an unlimited number of transactions off-chain. These transactions are almost instantaneous and incur negligible fees since they do not need to be recorded on the blockchain immediately."
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <TileExplainer
+                            //image={privacyanddecentralization}
+                            title="Closing the Channel"
+                            paragraph="When the parties decide to close the channel, the final balance of each party is recorded on the Bitcoin blockchain. This ensures that the integrity and security of the transactions are maintained."
+                        />
+                    </Grid>
+                </Grid>
+        </React.Fragment>
+        <React.Fragment>
+            <div>&nbsp;</div>
+        </React.Fragment>
+        <React.Fragment>
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <Typography variant="h1" component="h2">
+                        Benefits of Lightning <br/>
+                        Bitcoin Lightning Bitcoin <br/>
+                        offers several advantages <br/>
+                        over traditional Bitcoin <br/>
+                        transactions <br/>
+                    </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <TileExplainer
+                            image={transactionspeed}
+                            title="Speed"
+                            breakImageAndTitle={false}
+                            paragraph="When the parties decide to close the channel, the final balance of each party is recorded on the Bitcoin blockchain. This ensures that the integrity and security of the transactions are maintained."
+                    />
+                    <TileExplainer
+                            image={lowfees}
+                            title="Low Fees"
+                            breakImageAndTitle={false}
+                            paragraph="By minimizing the number of transactions recorded on the blockchain, Lightning Bitcoin reduces transaction fees, making microtransactions feasible."
+                    />
+                    <TileExplainer
+                            image={scalability}
+                            title="Scalability"
+                            breakImageAndTitle={false}
+                            paragraph="The Lightning Network can handle a significantly higher number of transactions per second compared to the Bitcoin blockchain, making it more scalable for widespread use."
+                    />
+                    <TileExplainer
+                            image={security}
+                            title="Security"
+                            breakImageAndTitle={false}
+                            paragraph="Despite being conducted off-chain, Lightning Bitcoin transactions retain the security features of the Bitcoin blockchain. The final settlement on the blockchain ensures the integrity and immutability of transactions."
+                    />
+                    <TileExplainer
+                            image={privacy}
+                            title="Privacy"
+                            breakImageAndTitle={false}
+                            paragraph="Since most transactions occur off-chain, users enjoy a higher degree of privacy compared to on-chain transaction."
+                    />
+                </Grid>
+            </Grid>
+        </React.Fragment>
+        <Footer />
         </React.Fragment>
     )
 }
