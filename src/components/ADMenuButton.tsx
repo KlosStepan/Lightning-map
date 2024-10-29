7e3d71df2fd6b6af26d23c9055ce0d4b3d1e2014
Stepan Klos <stepanklos@gmail.com>
2024-10-29 23:02:58 +0100

    Redesign sprint 78 - Admin Dashboard Menu Button refactor+styling

--- src/components/ADMenuButton.tsx
+++ src/components/ADMenuButton.tsx
@@ -1,63 +1,61 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import LoginArrowRight from '../img/login-arrow-right.png';
 import { Link } from 'react-router-dom';
+
+const linkStyle = {
+    color: "inherit",
+    textDecoration: "inherit",
+    fontSize: '18px',
+};
+
+const buttonStyle = {
+    backgroundColor: 'white',
+    borderRadius: '20px',
+    width: '100%',
+    padding: '4px 14px', // Top/Bottom 4px, Left/Right 14px
+    margin: '12px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '&:hover': {
+        // Optional: change color on hover
+    },
+};
+
+const boxContainerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    position: 'relative',
+};
+
+const iconStyle = {
+    width: 24,
+    height: 24,
+    marginRight: 2,
+};
+
+const titleStyle = {
+    textAlign: 'center',
+    flexGrow: 1,
+};
 
 type ADMenuButtonProps = {
     icon: string;
     title: string;
     path: string;
-}
+};
 
 const ADMenuButton: React.FC<ADMenuButtonProps> = ({ icon, title, path }) => {
     return (
         <React.Fragment>
-            <Link style={{ color: "inherit", textDecoration: "inherit", fontSize: '18px' }} to={path}>
-                {/*//Make Link*/}
-                <Button
-                    color="primary"
-                    sx={{
-                        backgroundColor: 'white',
-                        borderRadius: '20px',
-                        width: '100%',
-                        padding: '4px',
-                        marginTop: '12px',
-                        marginBottom: '12px',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        '&:hover': {
-                            // Optional: change color on hover
-                        },
-                    }}
-                >
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            width: '100%',
-                            position: 'relative',
-                        }}
-                    >
-                        <Box
-                            component="img"
-                            src={icon}
-                            alt={title}
-                            sx={{
-                                width: 24,
-                                height: 24,
-                                marginRight: 2,
-                            }}
-                        />
-                        <Box
-                            component="span"
-                            sx={{
-                                textAlign: 'center',
-                                flexGrow: 1,
-                            }}
-                        >
+            <Link style={linkStyle} to={path}>
+                <Button color="primary" sx={buttonStyle}>
+                    <Box sx={boxContainerStyle}>
+                        <Box component="img" src={icon} alt={title} sx={iconStyle} />
+                        <Box component="span" sx={titleStyle}>
                             {title}
                         </Box>
                     </Box>
@@ -65,6 +63,6 @@ const ADMenuButton: React.FC<ADMenuButtonProps> = ({ icon, title, path }) => {
             </Link>
         </React.Fragment>
     );
-}
+};
 
 export default ADMenuButton;
