37c3eb14f1d86c5e0fa2609200599a13e7029d07
Stepan Klos <stepanklos@gmail.com>
2024-11-08 16:10:22 +0100

    Redesign sprint 85 - 2 x Modal (Eshop Spot) prep.

--- src/forms/ModifFormEshop.tsx
+++ src/forms/ModifFormEshop.tsx
@@ -1,23 +1,69 @@
 import React from "react";
 
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Input from '@mui/material/Input';
+
+import ButtonUniversal from "../components/ButtonUniversal";
+import IEshop from "../ts/IEeshop";
+
+
 type ModifFormEshopProps = {
     //bool edit //true - loading stuff
+    edit: boolean;
     //eshop: IEshop
+    eshop?: IEshop
+    //FuncCancel (vv insert function from parent component to swap modalOpen to false in ^^ component)
 }
 
-const ModifFormEshop: React.FC<ModifFormEshopProps> = ({ }) => {
+const ModifFormEshop: React.FC<ModifFormEshopProps> = ({ edit = false }) => {
     //references
     //states
     //etc.
 
-    //useEffect(()=>{
+    //useEffect(() => {
     //  set all stuff from props(?)
     //})
-    
+    //FuncCancel (vv insert function from parent component to swap modalOpen to false in ^^ component)
+
     //function AddEshop()
+    const AddEshop = () => {
+        console.log("Adding eshop")
+    }
     //function UpdateEshop()
+    const UpdateEshop = () => {
+        console.log("Updating eshop")
+    }
+    //NOTE: if(edit)...LOAD
     return(
-        <div>-table of items of datatype eshop goes here-</div>
+        <React.Fragment>
+        <div>-table of items of datatype IEshop goes here-</div>
+        <Typography id="modal-modal-description" style={{ marginTop: '16px' }}>
+        <Box mt={2}>
+            <Typography variant="h2" component="h5">Title</Typography>
+            <Input placeholder="Title" fullWidth />
+          </Box>
+          <Box mt={2}>
+            <Typography variant="h2" component="h5">Description</Typography>
+            <Input placeholder="Description" fullWidth />
+          </Box>
+          <Box mt={2}>
+            <Typography variant="h2" component="h5">Web</Typography>
+            <Input placeholder="Web" fullWidth />
+          </Box>
+          <Box mt={2} sx={{ width: '100%', border: '1px solid #000' }}>Upload Img</Box>
+          {/* Action Buttons */}
+          <Box display="flex" justifyContent="flex-end" mt={2}>
+            {/*<ButtonUniversal title="Cancel changes" color="#8000FF" textColor="white" actionDelegate={FuncCancel} />*/}
+            <ButtonUniversal
+                title={edit ? "Save changes" : "Add Eshop"}
+                color={edit ? "#F23CFF" : "#8000FF"}
+                textColor="white"
+                actionDelegate={edit ? UpdateEshop : AddEshop}
+            />
+          </Box>
+        </Typography>
+        </React.Fragment>
     )
 }
 export default ModifFormEshop;
--- src/forms/ModifFormSpot.tsx
+++ src/forms/ModifFormSpot.tsx
@@ -1,23 +1,93 @@
 import React from "react";
 
+import Box from '@mui/material/Box';
+import Typography from "@mui/material/Typography";
+import Input from '@mui/material/Input';
+
+import ButtonUniversal from "../components/ButtonUniversal";
+import ToggleSocialInput from "../components/ToggleSocialInput";
+import IMerchant from "../ts/IMerchant";
+
 type ModifFormSpotProps = {
     //bool edit //true - loading stuff
-    //eshop: IMerchant   
+    edit: boolean;
+    //merchant: IMerchant
+    merchant?: IMerchant
+    //FuncCancel (vv insert function from parent component to swap modalOpen to false in ^^ component)
 }
 
-const ModifFormSpot: React.FC<ModifFormSpotProps> = ({ }) => {
+const ModifFormSpot: React.FC<ModifFormSpotProps> = ({ edit = false }) => {
     //references
     //states
     //etc.
 
-    //useEffect(()=>{
+    //useEffect(() => {
     //  set all stuff from props(?)
     //})
-    
+    //FuncCancel (vv insert function from parent component to swap modalOpen to false in ^^ component)
+
     //function AddSpot()
+    const AddSpot = () => {
+        console.log("Adding spot")
+    }
     //function UpdateSpot()
+    const UpdateSpot = () => {
+        console.log("Updating spot")
+    }
+    //NOTE: if(edit)...LOAD
     return(
-        <div>-table of items of datatype spot goes here-</div>
+        <React.Fragment>
+                    <div>-table of items of datatype IMerchant goes here-</div>
+                  <Box mt={2}>
+                  <Typography variant="h2" component="h5">Title</Typography>
+                  <Input placeholder="Title" fullWidth />
+                </Box>
+                <Box mt={2}>
+                  <Typography variant="h2" component="h5">Description</Typography>
+                  <Input placeholder="Description" fullWidth />
+                </Box>
+                <Box mt={2}>
+                  <Typography variant="h2" component="h5">Address</Typography>
+                  <Input placeholder="Address" fullWidth />
+                </Box>
+                <Box mt={2}>
+                  <Typography variant="h2" component="h5">City</Typography>
+                  <Input placeholder="City" fullWidth />
+                </Box>
+                <Box mt={2}>
+                  <Typography variant="h2" component="h5">Postal Code</Typography>
+                  <Input placeholder="Postal Code" fullWidth />
+                </Box>
+                <Box mt={2} sx={{ width: '100%', border: '1px solid #000' }}>Map</Box>
+                {/* Social Media Links directly in JSX */}
+                {/*<Box display="flex" mt={2}>*/}
+                <hr/>
+                {/*TODO - parametrized setter name->link to Dictionary up^ */}
+                <Box mt={2}>
+                  <ToggleSocialInput name={"IG"}/>
+                  <ToggleSocialInput name={"FB"}/>
+                  <ToggleSocialInput name={"X"}/>
+                  <ToggleSocialInput name={"Threads"}/>
+                </Box>
+                <hr/>
+                  {/*<Typography style={{ marginLeft: '8px' }}>+IG</Typography>
+                  <Typography style={{ marginLeft: '8px' }}>+FB</Typography>
+                  <Typography style={{ marginLeft: '8px' }}>+X</Typography>
+                  <Typography style={{ marginLeft: '8px' }}>+Threads</Typography>*/}
+                {/*</Box>*/}
+                <Box mt={2} sx={{ width: '100%', border: '1px solid #000' }}>Upload Img</Box>
+                {/* Action Buttons */}
+                <Box display="flex" justifyContent="flex-end" mt={2}>
+                  {/*<ButtonUniversal title="Cancel changes" color="#8000FF" textColor="white" actionDelegate={FuncCancel} />*/}
+                <ButtonUniversal
+                    title={edit ? "Save changes" : "Add spot"}
+                    color={edit ? "#F23CFF" : "#8000FF"}
+                    textColor="white"
+                    actionDelegate={edit ? UpdateSpot : AddSpot}
+                />
+                </Box>
+              {/*</Typography>*/}
+              </React.Fragment>
     )
 }
 export default ModifFormSpot;
