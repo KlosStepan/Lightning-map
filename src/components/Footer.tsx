5b4cd88b3c8d85e9eb6fb750bb016468d6c0a8d3
Stepan Klos <stepanklos@gmail.com>
2025-01-09 03:33:53 +0100

    Redesign sprint 125 - Footer refact+styling+resp. breakdown adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Grid, Typography } from '@mui/material';
 import { BorderTop } from "@mui/icons-material";
+import HrGreyCustomSeparator from "./HrGreyCustomSeparator";
 
 const footerStyling = {
     marginTop: '20px',
@@ -10,41 +11,42 @@ const footerStyling = {
 }
 
 type FooterProps = {
-
+    //
 }
 
 const Footer: React.FC<FooterProps> = ({ }) => {
     return (
         <React.Fragment>
-            <div style={{ marginTop: '40px', borderTop: '1px solid #808080' }}></div>
+            <HrGreyCustomSeparator/>
             <Grid container spacing={2} sx={{ ...footerStyling }}>
                 {/* First part: 1/2 */}
                 <Grid item xs={12} md={6}>
-                    <Typography variant="h1" /*align="center"*/>Experience the Power of
-                        Lightning Network Everywhere</Typography>
+                    <Typography component="h1" variant="h1" >
+                    Experience the Power of Lightning Network Everywhere
+                    </Typography>
                 </Grid>
                 {/* Second part: 1/4 */}
                 <Grid item xs={12} md={3}>
-                    <Typography variant="h2" /*align="center"*/>Support</Typography>
-                    <Typography variant="h3">support@lightningeverywhere.com</Typography>
+                <Typography component="h1" variant="h3" sx={{ color: '#6B7280' }}>Support</Typography>
+                    {/** */}
+                    <Typography component="h2" variant="h2">support@lightningeverywhere.com</Typography>
                 </Grid>
                 {/* Third part: 1/4 */}
                 <Grid item xs={12} md={3}>
-                    <Typography variant="h2">Follow us on</Typography>
-                    <Typography variant="h3">Instagram</Typography>
-                    <Typography variant="h3">Threads</Typography>
-                    <Typography variant="h3">Threads</Typography>
+                    <Typography component="h1" variant="h3" sx={{ color: '#6B7280' }}>Follow us on</Typography>
+                    {/** */}
+                    <Typography component="h2" variant="h2">Instagram</Typography>
+                    <Typography component="h2" variant="h2">X</Typography>
+                    <Typography component="h2" variant="h2">Threads</Typography>
                 </Grid>
-                {/*</Grid>
-            <Grid container spacing={2}>*/}
                 <Grid item xs={12} md={6}>
-                    <Typography variant="h3" component="h3">2024 © Lightning. Everywhere.</Typography>
+                    <Typography component="h3" variant="h3">design: smékal</Typography>
                 </Grid>
                 <Grid item xs={12} md={3}>
-                    <Typography variant="h3" component="h3">GDPR</Typography>
+                    <Typography component="h3" variant="h3">GDPR</Typography>
                 </Grid>
                 <Grid item xs={12} md={3}>
-                    <Typography variant="h3" component="h3">design: smékal</Typography>
+                    <Typography component="h3" variant="h3">2024 © Lightning. Everywhere.</Typography>
                 </Grid>
             </Grid>
         </React.Fragment>
