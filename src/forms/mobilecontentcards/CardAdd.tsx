4ad40719f794675dbca8aaaf93cff43b98564eec
Stepan Klos <stepanklos@gmail.com>
2025-01-13 03:52:00 +0100

    Redesign sprint 140 - Card for wrapping Add tiles for AD - TODO start working on AD

---
+++ src/forms/mobilecontentcards/CardAdd.tsx
@@ -0,0 +1,17 @@
+// CardAdd.tsx
+import React from 'react';
+
+interface CardAddProps {
+  // Add your specific props here
+}
+
+const CardAdd: React.FC<CardAddProps> = ({ }) => {
+  return (
+    <React.Fragment>
+      <div>...</div>
+      {/* Add your card add functionality here */}
+    </React.Fragment>
+  );
+};
+
+export default CardAdd;
