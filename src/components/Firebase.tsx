d18e4239a65ce8fecf67637734db857d66faa518
Stepan Klos <stepanklos@gmail.com>
2025-04-02 23:33:23 +0200

    Attempt to fix following Firebase: Error (auth/invalid-api-key)

--- src/components/Firebase.tsx
+++ src/components/Firebase.tsx
@@ -21,7 +21,7 @@ import {
 import { getStorage, FirebaseStorage } from "firebase/storage"; // Import Firebase Storage
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY  || "PUT_IN_A_DUMMY_API_KIEY",
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
