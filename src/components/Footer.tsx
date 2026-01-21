96f102dad56a168e2dbd597dc63597bc86b9909a
Stepan Klos <stepanklos@gmail.com>
2026-01-21 23:38:15 +0100

    chore: remove message about non-working e-mail in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,6 @@ const Footer: React.FC<FooterProps> = ( ) => {
                 <Grid item xs={12} md={3}>
                     <Typography component="h1" variant="h3" sx={{ color: '#6B7280' }}>Write us</Typography>
                     <Typography component="h2" variant="h2">stepan@lightningeverywhere.com</Typography>
-                    <Typography component="h2" variant="h4">// ^don't msg yet, might not be working</Typography>
                     <div>&nbsp;</div>
                     <div>&nbsp;</div>
                     {/*<Typography component="h6" variant="h6">
